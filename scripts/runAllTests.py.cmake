e89d6e81704f81a44eb6b5a601f04e1c106df3aa
AnastaZIuk <areklachowicz@gmail.com>
2023-10-02 21:38:12 +0200

    typo, in python comment is # not //

--- scripts/runAllTests.py.cmake
+++ scripts/runAllTests.py.cmake
@@ -13,8 +13,8 @@ import sys
 import os
 
 os.chdir(os.path.dirname(__file__))
-sys.path.append("@NBL_PYTHON_FRAMEWORK_MODULE_PATH_REL@") // relative path to Nabla Python Framework module, relocatable
-sys.path.append("@NBL_TEST_TARGET_MODULE_PATH_REL@") // relative path to a test target interface module, relocatable
+sys.path.append("@NBL_PYTHON_FRAMEWORK_MODULE_PATH_REL@") # relative path to Nabla Python Framework module, relocatable
+sys.path.append("@NBL_TEST_TARGET_MODULE_PATH_REL@") # relative path to a test target interface module, relocatable
 
 import test
-test.main() // each test target implements its testing interface by overriding common one in Nabla Python Framework module
+test.main() # each test target implements its testing interface by overriding common one in Nabla Python Framework module
