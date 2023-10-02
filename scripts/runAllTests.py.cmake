7070cbdf255b8ea9e52a9fd0e04045db02e3ef82
AnastaZIuk <areklachowicz@gmail.com>
2023-10-02 15:31:21 +0200

    add CMakeLists.txt, add Copyrights and script description in template scripts/runAllTests.py.cmake

--- scripts/runAllTests.py.cmake
+++ scripts/runAllTests.py.cmake
@@ -1,7 +1,18 @@
+# Copyright (C) 2023 - DevSH Graphics Programming Sp. z O.O.
+
+# "@EXECUTABLE_NAME@" target's test script, creates python environment for accessing 
+# Nabla Python Framework module and runs the target's testing interface implementation
+
+# Example of usage
+# 1) runAllTests.py
+#	- runs all profiles found, goes through all batches found in a profile being processed
+# 2) runAllTests.py <profile path/index> [OPTIONAL] <batch index/range of batches> ... [OPTIONAL] <batch index/range of batches>
+#	- runs given profile, if batch index (or range of batches) is given then it will process only the index of the batch (or the range)
+
 import sys
 import os
-os.chdir(os.path.dirname(__file__))
 
+os.chdir(os.path.dirname(__file__))
 sys.path.append("@NBL_PYTHON_FRAMEWORK_MODULE_PATH_REL@") // relative path to Nabla Python Framework module, relocatable
 sys.path.append("@NBL_TEST_TARGET_MODULE_PATH_REL@") // relative path to a test target interface module, relocatable
 
