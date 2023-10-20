8acceb1cb0764ca02547d8802b6c2e4a39413a00
AnastaZIuk <areklachowicz@gmail.com>
2023-10-20 14:09:31 +0200

    update CMakeLists.txt, create cmake/launch.json.cmake, rename scripts/runAllTests.py.cmake to scripts/__main__.py.cmake in order to turn a directory containing tests implementation into a python module

---
+++ cmake/launch.json.cmake
@@ -0,0 +1,13 @@
+{
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "@_NBL_PF_DESC_NAME_@",
+            "type": "python",
+            "request": "launch",
+            "module": "@_NBL_PF_MODULE_NAME_@",
+            "justMyCode": true,
+            "cwd": "${workspaceFolder}/../",
+        }
+    ]
+}
